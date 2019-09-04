527b86c34b4d6540b4ca9a686de475bd5e1f1b3b
guoyangnice <yangjienice@126.com>
2019-09-04 22:54:44 +0800

    添加导航栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="home">
-    <button type="primary" @click="modal1=true" id="back">返回</button>
-    <Modal v-model="modal1" title="Common Modal dialog box title" @on-ok="ok" @on-cancel="cancel">
-      <p>请确认您的选择</p>
-    </Modal>
-    <p id="username">{{this.$store.getters.getUsername}}</p>
+    <div id="header">
+      <button type="primary" @click="modal1=true" id="back">返回</button>
+      <Modal v-model="modal1" title="Common Modal dialog box title" @on-ok="ok" @on-cancel="cancel">
+        <p>请确认您的选择</p>
+      </Modal>
+      <p id="username">{{this.$store.getters.getUsername}}</p>
+    </div>
+    <div id="menu">
+        <Menu :theme="theme3" active-name="1">
+        <MenuGroup title="导航">
+            <MenuItem name="1">
+                <Icon type="md-document" />
+                todolist
+            </MenuItem>
+            <MenuItem name="2">
+                <Icon type="md-chatbubbles" />
+                我的
+            </MenuItem>
+        </MenuGroup>
+        </Menu>
+    </div>
   </div>
 </template>
 
@@ -29,16 +45,22 @@ export default {
 </script>
 
 <style scoped>
-#back {
-  text-align: left;
-  margin-top: 50px;
-  margin-left: 30px;
+
+ #back {
+  margin-top:50px; 
+  margin-left:50px;
   float: left;
 }
 #username {
-  text-align: right;
-  margin-top: 50px;
-  margin-right: 30px;
+
+  margin-top:50px; 
+  margin-right:50px;
   float: right;
+} 
+#menu {
+  
+  float:left;
+  margin-top:100px; 
+  margin-left:-50px;
 }
 </style>
